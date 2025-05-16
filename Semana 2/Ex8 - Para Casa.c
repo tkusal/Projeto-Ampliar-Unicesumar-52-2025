6580b5427d1d2eb199a106a9d3d1ef5b0e9d916e
tkusal <thiago.kusal@outlook.com>
2025-05-15 22:14:52 -0300

    Aula 15/05/2025 - retif

--- Semana 2/Ex8 - Para Casa.c
+++ Semana 2/Ex8 - Para Casa.c
@@ -1,4 +1,4 @@
 /*
-    Escreva um programa em C que leia a medida dos 3 lados de uma forma.
+    Escreva um programa em C que leia a medida dos 3 lados de uma forma geométrica.
     O programa deverá usar o teorema de Pitágoras para definir se esta forma é um triângulo.
 */
